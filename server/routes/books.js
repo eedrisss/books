4a128e93cd347089f68d54b8dbf63b6f9b974fc9
IDRIs <imustapha30@yahoo.com>
2023-11-08 09:55:36 -0500

    DB changes

--- server/routes/books.js
+++ server/routes/books.js
@@ -27,7 +27,7 @@ router.get('/', (req, res, next) => {
 router.get('/add', (req, res, next) => {
 
   const bookId = req.params.id;
-  res.render('books/details', { title: 'Book Details', books: book });
+  res.render('books/details', { Title: 'Book Details', books: book });
 });
 
 // POST process the Book Details page and create a new Book - CREATE
@@ -59,41 +59,55 @@ router.post('/add', (req, res, next) => {
 });
 
 // GET - display the details of a book and provide an option to edit
-router.get('/details/:id', (req, res, next) => {
-  const bookId = req.params.id;
+router.get('/:id', (req, res, next) => {
 
-  // Find the book by ID in the database
-  book.findById(bookId, (err, book) => {
-    if (err) {
-      console.error(err);
-    } else {
-      res.render('details', {
-        title: 'Book Details',
-        book: book
-      });
-    }
-  });
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+  try {
+    book.findOne({ _id: req.params.id })
+            .then((data) => {
+              if (data) {
+                res.render('books/details', { title: 'Edit Book', books: data });
+              }
+              else {
+                console.error(`Trying to edit a book that doesn't exist: id=[${req.body.id}]`);
+                res.status(500).redirect('/books');
+              }
+            });
+  } catch (exception) {
+    console.error(`Failed to find the book by id=[${req.body.id}]: ${exception}`);
+    res.status(500).redirect('/books');
+  }
 });
 
 // POST - process the information passed from the details form and update the document
-router.post('/details/:id', (req, res, next) => {
-  const bookId = req.params.id;
+router.post('/:id', (req, res, next) => {
 
-  // Find the book by ID and update its details
-  book.findByIdAndUpdate(bookId, {
-    Title: req.body.title,
-    Description: req.body.description,
-    Price: req.body.price,
-    Author: req.body.author,
-    Genre: req.body.genre
-  }, (err) => {
-    if (err) {
-      console.error(err);
-    } else {
-      res.redirect('/books'); // Redirect to the books list after updating the book
-    }
-  });
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+let updatedBook = book({
+  "_id": req.body.id,
+  "Title": req.body.title,
+  "Description": req.body.description,
+  "Price": req.body.price,
+  "Author": req.body.author,
+  "Genre": req.body.genre
 });
+
+book.findOneAndUpdate({ _id: req.body.id }, updatedBook )
+        .then((result) => {
+          if (result) {
+            res.redirect('/books');
+          }
+          else {
+            console.error(`Failed to update the book: id=[${req.body.id}]`);
+            res.status(500).redirect('/books');
+          }
+        });
+});
+
 
 // GET - process the delete by user id
 router.get('/delete/:id', (req, res, next) => {
